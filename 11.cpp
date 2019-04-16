134bdff3f7b7ff812aa0a6d62fcceef9366720b6
Bhavik Kothari <bkkothari2255@gmail.com>
2019-04-16 20:32:58 +0530

    Solutions
    Prefinal Question Bank Solution of Program

--- 11.cpp
+++ 11.cpp
@@ -3,12 +3,10 @@
 using namespace std; 
 inline int large(int n1,int n2,int n3) 
 {
-	return n3 < fmax(n1,n2)? fmax(n1,n2) : n3;
-	/*
+	//return n3 < fmax(n1,n2)? fmax(n1,n2) : n3;
   	  if(n1>n2 && n1>n3)	  return n1;
 	  else if(n2>n1 && n2>n3) return n2;
 	  else  return n3;
-	*/
 }
 int main()
 { 
